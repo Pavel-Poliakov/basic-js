5d766437049936a41ccb90052d99d58a5869cf40
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-04-15 19:58:32 +0400

    fix:transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -18,15 +18,28 @@ function transform(array) {
     let a = "'arr' parameter must be an instance of the Array!"
     return a;
   }
-  else{
+  else if(array.includes('--discard-next') || array.includes('--discard-prev')){
     let arr = Array.from(array);
+    let arr2 = Array.from(array);
+    let a = arr.length -1;
     arr.forEach((el,index)=>{
-      if(el==='--discard-next'){arr.splice(index,2)};
-      if(el==='--discard-prev'){arr.splice(index-1,2)};
-      if(el==='--double-next'){arr[index] = arr[index+1]};
-      if(el==='--double-prev'){arr[index] = arr[index-1]};
-  })
-    return arr
+      el==='--discard-next'&& el != arr[a]?arr.splice(index,2):false
+      el==='--discard-prev'&& el != arr[0]?arr.splice(index-1,2):false
+    })
+    if(arr.length != arr2.length){
+      return arr
+    }
+}
+else if(array.includes('--double-next') || array.includes('--double-prev')){
+  let arr = Array.from(array);
+    let a = arr.length -1;
+    arr.forEach((el,index)=>{
+      el==='--double-next' && el != arr[a]?arr[index] = arr[index+1]:false;
+      el==='--double-prev' && el != arr[0]?arr[index] = arr[index-1]:false;
+    })
+    if(!arr.includes('--double-next') && !arr.includes('--double-prev')){
+      return arr
+    }
 }
 }
 
@@ -34,7 +47,10 @@ module.exports = {
   transform
 };
 
-// console.log(transform([1, 2, 3, '--double-prev', 4, 5]))
+// console.log(transform([1, 2, 3, '--discard-next']))
+// console.log(transform(['--discard-prev',5,6]))
+// console.log(transform([1, 2, 3, '--double-next']))
+// console.log(transform(['--double-prev', 4, 5]))
 // Ваша задача — реализовать функцию transform(arr), которая принимает arrayи возвращает преобразованный массив на основе содержащихся в нем управляющих последовательностейarr . Последовательности управления являются определенными stringэлементами упомянутого array:
 
 // --discard-nextисключает следующий элемент массива из преобразованного массива.
