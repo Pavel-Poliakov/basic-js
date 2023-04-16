27068e9fd0d84fe08449c83edc271f0d2c43e7ff
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-04-16 21:07:50 +0400

    fix: transform-array pre version

--- src/transform-array.js
+++ src/transform-array.js
@@ -17,30 +17,23 @@ function transform(array) {
   if(!Array.isArray(array)){
     let a = "'Arr' parameter must be an instance of the Array!"
     return a;
+  } 
+  else if(array.length == 0){
+    return array
+  }else if(array.length>0 && (array.includes('--discard-next') || array.includes('--discard-prev') || array.includes('--double-next') || array.includes('--double-prev'))){
+    let arrCopy = Array.from(array).flat(Infinity);
+    if(arrCopy.includes('--discard-next') || arrCopy.includes('--discard-prev') || arrCopy.includes('--double-next') || arrCopy.includes('--double-prev')){
+      let a = arrCopy.length -1;
+      arrCopy.forEach((el,index)=>{
+        el==='--discard-next'&& el != arrCopy[a]?arrCopy.splice(index,2):false;
+        el==='--discard-prev'&& el != arrCopy[0]?arrCopy.splice(index-1,2):false;
+        el==='--double-next' && el != arrCopy[a]?arrCopy[index] = arrCopy[index+1]:false;
+        el==='--double-prev' && el != arrCopy[0]?arrCopy[index] = arrCopy[index-1]:false;
+      })
+    }
+   return !arrCopy.includes('--double-next') && !arrCopy.includes('--double-prev') &&
+    !arrCopy.includes('--discard-next') && !arrCopy.includes('--discard-prev') ? arrCopy:undefined;
   }
-  else if(array.includes('--discard-next') || array.includes('--discard-prev')){
-    let arr = Array.from(array);
-    let arr2 = Array.from(array);
-    let a = arr.length -1;
-    arr.forEach((el,index)=>{
-      el==='--discard-next'&& el != arr[a]?arr.splice(index,2):false
-      el==='--discard-prev'&& el != arr[0]?arr.splice(index-1,2):false
-    })
-    if(arr.length != arr2.length){
-      return arr
-    }
-}
-else if(array.includes('--double-next') || array.includes('--double-prev')|| array.length >= 0){
-  let arr = Array.from(array);
-    let a = arr.length -1;
-    arr.forEach((el,index)=>{
-      el==='--double-next' && el != arr[a]?arr[index] = arr[index+1]:false;
-      el==='--double-prev' && el != arr[0]?arr[index] = arr[index-1]:false;
-    })
-    if(!arr.includes('--double-next') && !arr.includes('--double-prev')){
-      return arr
-    }
-}
 }
 
 module.exports = {
