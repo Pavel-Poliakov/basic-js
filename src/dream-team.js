f69834b87c39ac58b0f7ac80191c0e9f29b7b26b
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-04-16 13:54:26 +0400

    fix: lower and " "

--- src/dream-team.js
+++ src/dream-team.js
@@ -17,13 +17,28 @@ function createDreamTeam(members) {
   let arr = [];
   let arr2 = [];
   if(Array.isArray(members) && members.length>0){
-    members.forEach(el=>{
+    members.flat(Infinity).forEach(el=>{
       typeof el === 'string'? arr.push(el.split('')):false;
     })
+      //  arr.forEach(el=>{
+      //   el.some(item=>{item !== ' ' && item === item.match(/[a-zA-Z]/g) ?arr2.push(item):false})
+      //  })
        arr.forEach(el=>{
-        el.some(item=>{item === item.toUpperCase() && item !== ' ' && item !== item.toLowerCase() ?arr2.push(item):false})
+        // el.forEach(item=>item !== ' ' && item.match(/[a-zA-Z]/i)?arr2.push(item):false)
+        for(let i = 0; el.length>i; i++){
+          if(el[i] !== ' ' && el[i].match(/[a-zA-Z]/i)){
+            arr2.push(el[i])
+            break
+          }
+        }
        })
-       return arr2.length>0?arr2.sort().join(''):false
+      //  return arr2.length>0?arr2.sort().join(''):false
+       return arr2.length>0?arr2.sort().join('').toUpperCase():false
+      // if(arr2.length>0){
+      //   return arr2.sort().join('')
+      // }else{
+      //   return false
+      // }
   }else{
     return false
   }
@@ -31,4 +46,37 @@ function createDreamTeam(members) {
 module.exports = {
   createDreamTeam
 };
-// console.log(createDreamTeam(['Llivia','         D', 1111, 'Lily', 'Oscar', true, null]))
+
+// console.log(
+//   createDreamTeam([
+//     "Amelia",
+//     null,
+//     undefined,
+//     "Ruby",
+//     "Lily",
+//     11,
+//     "Grace",
+//     22,
+//     "Millie",
+//     "Daisy",
+//     true,
+//     "Freya",
+//     false,
+//     "Erin",
+//     new Set([1, 2, 3, 4, 5]),
+//     "Megan",
+//     {
+//       John: "Smith"
+//     },
+//     "Jasmine",
+//     1,
+//     2,
+//     3,
+//     4,
+//     5,
+//     "Brooke"
+//   ])
+// );
+
+
+
