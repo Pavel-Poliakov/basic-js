3d1708ac437c46f978eb5a998f90d544cf6a4266
Pavel-Poliakov <polaykovp.s@gmail.com>
2023-04-17 12:36:11 +0400

    fix:transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(array) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function transform(array) {
+//   // throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
 //   if(!Array.isArray(array)){
 //     let a = "'Arr' parameter must be an instance of the Array!"
 //     return a;
@@ -43,8 +43,50 @@ function transform(array) {
 //       return arr
 //     }
 // }
+// }
+
+
+
+function transform(array) {
+  if(!Array.isArray(array)){
+    throw new NotImplementedError("'Arr' parameter must be an instance of the Array!");
+  } 
+  else if(array.length == 0){
+    return array
+  }else if(!array.includes('--double-next') && !array.includes('--double-prev') &&
+  !array.includes('--discard-next') && !array.includes('--discard-prev')){
+    return array
+  }
+  else if(array.length>0 && (array.includes('--discard-next') || array.includes('--discard-prev') || array.includes('--double-next') || array.includes('--double-prev'))){
+    let arrCopy = Array.from(array);
+    if(arrCopy.includes('--discard-next') || arrCopy.includes('--discard-prev') || arrCopy.includes('--double-next') || arrCopy.includes('--double-prev')){
+      let a = arrCopy.length -1;
+      arrCopy.forEach((el,index)=>{
+        if(el==='--discard-next'&& el != arrCopy[a]){
+          arrCopy.splice(index,2);
+        }else if(el==='--discard-next'&& el === arrCopy[a]){
+          arrCopy.splice(index,1);
+        }else if(el==='--discard-prev'&& el != arrCopy[0]){
+          arrCopy.splice(index-1,2);
+        }else if(el==='--discard-prev'&& el === arrCopy[0]){
+          arrCopy.splice(index,1);
+        }else if(el==='--double-next' && el != arrCopy[a]){
+          arrCopy[index] = arrCopy[index+1]
+        }else if(el==='--double-next' && el === arrCopy[a]){
+          arrCopy.splice(index,1);
+        }else if(el==='--double-prev' && el != arrCopy[0]){
+          arrCopy[index] = arrCopy[index-1];
+        }else if(el==='--double-prev' && el === arrCopy[0]){
+          arrCopy.splice(index,1);
+        }
+      })
+    }
+   return !arrCopy.includes('--double-next') && !arrCopy.includes('--double-prev') &&
+    !arrCopy.includes('--discard-next') && !arrCopy.includes('--discard-prev') ? arrCopy:undefined;
+  }else{
+    return undefined
+  }
 }
 module.exports = {
   transform
 };
-
